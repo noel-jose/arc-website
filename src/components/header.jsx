5089429d8636fe86b844291bddcc4c507051b605
Noel Jose <noeljose@pop-os.localdomain>
2022-09-23 11:03:52 +0530

    Modified style of the header section

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,17 +4,17 @@ export const Header = (props) => {
   return (
     <header id="header">
       <div className="intro">
-        <div className="container">
-          <div className="row">
+        <div className="container text-center">
+          <div className="col">
             <div className="col-md-8 col-md-offset-2 header-content">
-              <div className="intro-text">
+              <div className="intro-text ">
                 <h1>
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
               </div>
-              <Svg className="header-svg" />
+              {/* <Svg className="header-svg" /> */}
             </div>
           </div>
         </div>
